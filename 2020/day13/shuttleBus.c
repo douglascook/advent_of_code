68aaa5d3db78543d959e66f68741378408eed68d
Douglas Cook <douglas@kheironmed.com>
2020-12-18 17:36:56 +0000

    Day 13 - brute force part 2, test inputs working

--- 2020/day13/shuttleBus.c
+++ 2020/day13/shuttleBus.c
@@ -4,8 +4,10 @@
 #include "../lib/input.h"
 
 void getBuses(char *start, int *buses, int *busCount);
+void getBusesAndIndexes(char *start, int *buses, int *indexes);
 int compareInt(const void* a, const void* b);
-void checkFirstBusWeCanCatch(int *buses, int busCount, int departureTime);
+void checkFirstBusToCatch(int *buses, int busCount, int departureTime);
+void checkAllSubsequentDepartures(int *buses, int *indexes, int busCount);
 
 int main() {
   char input[1000], *start;
@@ -14,10 +16,16 @@ int main() {
   int departureTime = strtol(input, &start, 10);
   printf("Departure time is %d\n", departureTime);
 
-  int buses[200], busCount = 0;
+  int buses[200], indexes[200], busCount = 0;
   getBuses(start, buses, &busCount);
+  checkFirstBusToCatch(buses, busCount, departureTime);
 
-  checkFirstBusWeCanCatch(buses, busCount, departureTime);
+  getBusesAndIndexes(start, buses, indexes);
+  int i, j;
+  for (i = 0; i < busCount; i++) {
+    printf("Bus %d is at index %d\n", buses[i], indexes[i]);
+  }
+  checkAllSubsequentDepartures(buses, indexes, busCount);
 }
 
 
@@ -39,7 +47,29 @@ void getBuses(char *start, int *buses, int *busCount) {
   qsort(buses, *busCount, sizeof(int), compareInt);
 }
 
-void checkFirstBusWeCanCatch(int *buses, int busCount, int departureTime) {
+void getBusesAndIndexes(char *start, int *buses, int *indexes) {
+  char *next;
+  int busId, index = 0, busCount = 0;
+  while (*start != '\0') {
+    busId = strtol(start, &next, 10);
+    if (*start == ',') {
+      index++;
+    }
+    // if we're at an integer then add it to the array of bus IDs
+    if (busId != 0) {
+      buses[busCount] = busId;
+      indexes[busCount] = index;
+      busCount++;
+      start = next;
+    // otherwise increment pointer so we continue at next char
+    } else {
+      start = start + 1;
+    }
+  }
+
+}
+
+void checkFirstBusToCatch(int *buses, int busCount, int departureTime) {
   int earliest = INT_MAX, busId, j, nextDeparture, earliestBusId;
   for (int b = 0; b < busCount; b++) {
     busId = buses[b];
@@ -55,6 +85,31 @@ void checkFirstBusWeCanCatch(int *buses, int busCount, int departureTime) {
   }
   printf("The earliest bus we can catch is %d\n", earliestBusId);
   printf("and will leave %d minutes after our desired departure time\n", earliest - departureTime);
+}
+
+void checkAllSubsequentDepartures(int *buses, int *indexes, int busCount) {
+  // for each multiple of the bus with highest ID
+  //    check that multiple % earlier bus = highest bus index - bus being checked index
+  //    check that multiple % later bus = later bus - (later bus - bus being checked)
+  //
+  long long time, i;
+  int j;
+  int good = 0;
+  for (i = 1; !good; i++) {
+    time = buses[0] * i;
+    good = 1;
+
+    if (i % 100000 == 0)
+      printf("Checking time %lld\n", time);
+
+    for (j = 1; j < busCount; j++) {
+      if ((time + indexes[j]) % buses[j] != 0) {
+        good = 0;
+        break;
+      }
+    }
+  }
+  printf("Done! Timestamp is %lld\n", time);
 }
 
 int compareInt(const void* a, const void* b) {
