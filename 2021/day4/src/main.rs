694dd9051787176a890c130cfb071985a435b052
Douglas Cook <douglas@kheironmed.com>
2021-12-05 10:05:53 +0000

    Refactor using hashmap

--- 2021/day4/src/main.rs
+++ 2021/day4/src/main.rs
@@ -1,81 +1,7 @@
+use std::collections::HashMap;
 use std::fs;
 
 const BOARD_SIZE: usize = 5;
-
-#[derive(Debug)]
-struct Number {
-    value: usize,
-    marked: bool,
-}
-
-#[derive(Debug)]
-struct BingoBoard {
-    numbers: Vec<Vec<Number>>,
-}
-
-impl BingoBoard {
-    fn from_string(string: &str) -> BingoBoard {
-        let mut numbers: Vec<Vec<Number>> = Vec::new();
-        for row in string.trim().split('\n') {
-            numbers.push(
-                row.split_whitespace()
-                    .map(|n| Number {
-                        value: n.parse().expect("Must be a number"),
-                        marked: false,
-                    })
-                    .collect(),
-            );
-        }
-        BingoBoard { numbers }
-    }
-
-    /// Update the board, marking any numbers matching the called number
-    fn update(&mut self, called_number: usize) {
-        let mut updated = false;
-        for i in 0..BOARD_SIZE {
-            if updated {
-                return;
-            }
-            for j in 0..BOARD_SIZE {
-                if self.numbers[i][j].value == called_number {
-                    self.numbers[i][j].marked = true;
-                    updated = true;
-                    break;
-                }
-            }
-        }
-    }
-
-    /// A board is a winner if any column or row is completely marked
-    fn is_winner(&self) -> bool {
-        for i in 0..BOARD_SIZE {
-            if self.numbers[i].iter().all(|n| n.marked) {
-                // println!("Row {} is a winner!", i);
-                return true;
-            }
-        }
-        for j in 0..BOARD_SIZE {
-            if self.numbers.iter().all(|r| r[j].marked) {
-                // println!("Column {} is a winner!", j);
-                return true;
-            }
-        }
-        false
-    }
-
-    /// Calculate score for the board = sum of all unmarked numbers
-    fn get_score(&self) -> usize {
-        let mut total: usize = 0;
-        for i in 0..BOARD_SIZE {
-            for j in 0..BOARD_SIZE {
-                if !self.numbers[i][j].marked {
-                    total += self.numbers[i][j].value;
-                }
-            }
-        }
-        total
-    }
-}
 
 fn main() {
     println!("Advent of Code Day 4 - Squid (Bingo) Game!");
@@ -99,12 +25,15 @@ fn play_game(filepath: &str) {
         .map(|n| n.parse().expect("Must be number"))
         .collect();
 
+    // Keep track of which numbers have been called
+    let mut called_numbers: HashMap<usize, bool> = draw.iter().map(|&n| (n, false)).collect();
+
     let mut boards: Vec<BingoBoard> = boards_string
         .split("\n\n")
         .map(BingoBoard::from_string)
         .collect();
 
-    let scores = calculate_scores(&mut boards, draw);
+    let scores = calculate_scores(&mut boards, draw, &mut called_numbers);
     let winner = scores.first().expect("Must be a winner");
     println!(
         "Winning board is board {}, Unmarked sum = {}, Final score = {}",
@@ -122,21 +51,26 @@ fn play_game(filepath: &str) {
 }
 
 /// Update boards as numbers are called until all boards are complete and return score for each board
-fn calculate_scores(boards: &mut Vec<BingoBoard>, draw: Vec<usize>) -> Vec<(usize, usize, usize)> {
+fn calculate_scores(
+    boards: &mut Vec<BingoBoard>,
+    draw: Vec<usize>,
+    called_numbers: &mut HashMap<usize, bool>,
+) -> Vec<(usize, usize, usize)> {
     let mut scores: Vec<(usize, usize, usize)> = Vec::new();
     let mut completed: Vec<usize> = Vec::new();
     let board_count = boards.len();
 
-    for called_number in draw {
+    for number in draw {
+        called_numbers.insert(number, true);
+
         for board_index in 0..board_count {
             // skip any boards that have already won
             if !completed.contains(&board_index) {
                 let board = &mut boards[board_index];
-                board.update(called_number);
-                if board.is_winner() {
+                if board.is_winner(&called_numbers) {
                     completed.push(board_index);
-                    let score = board.get_score();
-                    scores.push((board_index, score, score * called_number));
+                    let score = board.get_score(&called_numbers);
+                    scores.push((board_index, score, score * number));
                 }
             }
         }
@@ -146,4 +80,60 @@ fn calculate_scores(boards: &mut Vec<BingoBoard>, draw: Vec<usize>) -> Vec<(usiz
         }
     }
     scores
+}
+
+#[derive(Debug)]
+struct BingoBoard {
+    numbers: Vec<Vec<usize>>,
+}
+
+impl BingoBoard {
+    fn from_string(string: &str) -> BingoBoard {
+        let mut numbers: Vec<Vec<usize>> = Vec::new();
+        for row in string.trim().split('\n') {
+            numbers.push(
+                row.split_whitespace()
+                    .map(|n| n.parse().expect("Must be a number"))
+                    .collect(),
+            );
+        }
+        BingoBoard { numbers }
+    }
+
+    /// A board is a winner if any column or row is completely marked
+    fn is_winner(&self, called_numbers: &HashMap<usize, bool>) -> bool {
+        for i in 0..BOARD_SIZE {
+            if self.numbers[i]
+                .iter()
+                .all(|n| *called_numbers.get(n).expect("Key exists"))
+            {
+                // println!("Row {} is a winner!", i);
+                return true;
+            }
+        }
+        for j in 0..BOARD_SIZE {
+            if self
+                .numbers
+                .iter()
+                .all(|row| *called_numbers.get(&row[j]).expect("Key exists"))
+            {
+                // println!("Column {} is a winner!", j);
+                return true;
+            }
+        }
+        false
+    }
+
+    /// Calculate score for the board = sum of all unmarked numbers
+    fn get_score(&self, called_numbers: &HashMap<usize, bool>) -> usize {
+        let mut total: usize = 0;
+        for i in 0..BOARD_SIZE {
+            for j in 0..BOARD_SIZE {
+                if !called_numbers.get(&self.numbers[i][j]).expect("Key exists") {
+                    total += self.numbers[i][j];
+                }
+            }
+        }
+        total
+    }
 }
