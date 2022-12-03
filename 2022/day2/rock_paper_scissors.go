dae8b703a7522f2b5bb0111b19397013877ea09a
Douglas Cook <douglas@kheironmed.com>
2022-12-03 09:51:53 +0000

    Simplify logic using modular arithmetic

--- 2022/day2/rock_paper_scissors.go
+++ 2022/day2/rock_paper_scissors.go
@@ -8,15 +8,16 @@ import (
 	"strings"
 )
 
-const rock = "rock"
-const paper = "paper"
-const scissors = "scissors"
+const rock = 0
+const paper = 1
+const scissors = 2
+
 const win = "win"
 const lose = "lose"
 const draw = "draw"
 
 func main() {
-	fmt.Println("Day 1 - Counting calories!")
+	fmt.Println("Day 2 - Rock Paper Scissors, it's a fix!")
 
 	originalScore := 0
 	maxFitchingScore := 0
@@ -35,62 +36,42 @@ func main() {
 	}
 	fmt.Println("Total score with original translation =", originalScore)
 	fmt.Println("Total score with max fitching results =", maxFitchingScore)
-
 }
 
-func scoreRound(me, you string) int {
+func scoreRound(me, you int) int {
+	// Initial score is based on value of the option you selected
+	scoreFromSelected := me + 1
+
 	var score int
-
-	// Initial score from what you selected
-	switch me {
-	case rock:
-		score = 1
-	case paper:
-		score = 2
-	case scissors:
-		score = 3
-	}
-
 	// Draw = 3 points
 	if me == you {
-		score += 3
-		// Win = 6 points
-	} else if (me == rock && you == scissors) || (me == scissors && you == paper) || (me == paper && you == rock) {
-		score += 6
+		score = scoreFromSelected + 3
+		// Win (each RPS in cycle always beats the one before) = 6 points
+	} else if me == (you+1)%3 {
+		score = scoreFromSelected + 6
+		// Loss = 0 points
+	} else {
+		score = scoreFromSelected
 	}
-	// Loss = 0 points
 
 	return score
 }
 
-func selectRpsToMatchResult(opponent, result string) string {
-	var rps string
-	if result == win {
-		switch opponent {
-		case rock:
-			rps = paper
-		case paper:
-			rps = scissors
-		case scissors:
-			rps = rock
-		}
-	} else if result == lose {
-		switch opponent {
-		case rock:
-			rps = scissors
-		case paper:
-			rps = rock
-		case scissors:
-			rps = paper
-		}
-	} else {
-		// Draw you choose the same as your opponent
+func selectRpsToMatchResult(opponent int, result string) int {
+	var rps int
+	switch result {
+	case win:
+		rps = (opponent + 1) % 3
+	case lose:
+		// -1 % 3 != 2 with go's mod operator add 3 so we always have a positive value to mod
+		rps = ((opponent - 1) + 3) % 3
+	case draw:
 		rps = opponent
 	}
 	return rps
 }
 
-func translateToRps(letter string) string {
+func translateToRps(letter string) int {
 	if letter == "A" || letter == "X" {
 		return rock
 	}
